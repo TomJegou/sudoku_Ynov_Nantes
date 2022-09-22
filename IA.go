5fc65f03be6ffc25c8224b5e81b2fb236586eb1e
laremi <91208870+Godlonx@users.noreply.github.com>
2022-09-22 22:19:19 +0200

    AI advencement

--- IA.go
+++ IA.go
@@ -1,21 +1,31 @@
 package sudoku
 
-func ResolveGrid(grid [][]int) [][]int {
+func ResolveGrid(grid [][][]int) [][][]int {
 	// Function that resolve the sudoku's grid that give in argument
 	/* The function fill the empty cell who have only 1 possible value. If there is no empty cell with only
 	1 possible value, it take the cell who have the least possible value and try the first possible value. */
-	var hardlogic bool = false
-	for i := 0; i < 2; i++ {
-		for line, tab := range grid {
-			for column, val := range tab {
-				if len(val) == 1 {
-					// Add the only possible value
-					grid[line][column] = val[0]
-				} else if len(val) > 0 && hardlogic {
-
+	var (
+		minPossible int = 9
+		minPos      []int
+		found       bool = false
+	)
+	for line, tab := range grid {
+		for column, val := range tab {
+			if len(val) > 1 {
+				if len(val) < minPossible {
+					minPossible = len(val)
+					minPos = []int{line, column}
+					found = true
 				}
 			}
 		}
+	}
+	if found {
+		grid[minPos[0]][minPos[1]] = []int{grid[minPos[0]][minPos[1]][0]}
+		// ReTest all possibilities for all empty cell
+		return ResolveGrid(grid)
+	} else {
+		return grid
 	}
 }
 
