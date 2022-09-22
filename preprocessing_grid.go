ae3eda92b207b2cd1ac51a38c724fe894460d83e
tomrigolo <tomjegou10@gmail.com>
2022-09-22 10:27:46 +0200

    Add Func Preprocess_grid

--- preprocessing_grid.go
+++ preprocessing_grid.go
@@ -1,1 +1,20 @@
 package sudoku
+
+import "strconv"
+
+func Preprocess_grid(s []string) []int {
+	table := []int{}
+	for y := 0; y < len(s); y++ {
+		a := []int{}
+		for x := 0; x < len(s[y]); x++ {
+			if string(s[y][x]) == "." {
+				a = append(a, -1)
+			} else {
+				val, _ := strconv.Atoi(string(s[y][x]))
+				a = append(a, val)
+			}
+		}
+		table = append(table, a...)
+	}
+	return table
+}
