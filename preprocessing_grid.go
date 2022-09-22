87f67fd5a73742c0993946a4bc96c007ac1be8ad
tomrigolo <tomjegou10@gmail.com>
2022-09-22 11:45:01 +0200

    instead of putting -1, we putt the number of possibility

--- preprocessing_grid.go
+++ preprocessing_grid.go
@@ -2,6 +2,21 @@ package sudoku
 
 import "strconv"
 
+func compute_number_of_possibility(s []string, x int, y int) int {
+	result := -9
+	for i := 0; i < len(s[y]); i++ {
+		if string(s[y][i]) != "." {
+			result += 1
+		}
+	}
+	for i := 0; i < len(s); i++ {
+		if string(s[i][x]) != "." {
+			result += 1
+		}
+	}
+	return result
+}
+
 func PreprocessGrid(s []string) [][]int {
 	//function that transform the grid given as argument
 	//in order to make simpler the data process for the IA
@@ -10,7 +25,7 @@ func PreprocessGrid(s []string) [][]int {
 		a := []int{}
 		for x := 0; x < len(s[y]); x++ {
 			if string(s[y][x]) == "." {
-				a = append(a, -1)
+				a = append(a, compute_number_of_possibility(s, x, y))
 			} else {
 				val, _ := strconv.Atoi(string(s[y][x]))
 				a = append(a, val)
