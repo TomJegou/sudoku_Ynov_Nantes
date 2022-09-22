e26561fd1add42253b73109599539e8fa2902e8a
letienne <etienne.lemee@ynov.com>
2022-09-22 16:27:47 +0200

    returns false if their is 2 times the same number in a line, or in the other case  returns true

--- grid_verification.go
+++ grid_verification.go
@@ -1,1 +1,17 @@
 package sudoku
+
+func Testvalsudoku(ligne []int)bool { 
+	lentab := len(ligne)
+	n := []int{}
+	for i := 0; i < lentab; i++ { 
+		for j := 0; j < len(n); j++ {
+			if ligne[i] == n[j] { // check if i and j are the same or not, if yes, it returns false
+				return false
+			} else {
+				n = append(n, ligne[i]) //puts ligne[i] in n 
+		}
+	} 
+	 
+	}	
+	return true 
+}
