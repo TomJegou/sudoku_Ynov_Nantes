990307c65097d330d15b45bc626b168d28f9121e
laremi <91208870+Godlonx@users.noreply.github.com>
2022-09-22 20:30:57 +0200

    Rework on the Resolve script

--- IA.go
+++ IA.go
@@ -4,17 +4,26 @@ func ResolveGrid(grid [][]int) [][]int {
 	// Function that resolve the sudoku's grid that give in argument
 	/* The function fill the empty cell who have only 1 possible value. If there is no empty cell with only
 	1 possible value, it take the cell who have the least possible value and try the first possible value. */
-	var (
-		line   int
-		column int
-	)
-	for line = 0; line < len(grid); line++ {
-		for column = 0; column < len(grid[0]); column++ {
-			if grid[line][column] == -1 {
-				// Fill the cell with the only possible value
-				// grid[line][column] = ** Function who retrun the first possible value **
+	var hardlogic bool = false
+	for i := 0; i < 2; i++ {
+		for line, tab := range grid {
+			for column, val := range tab {
+				if len(val) == 1 {
+					// Add the only possible value
+					grid[line][column] = val[0]
+				} else if len(val) > 0 && hardlogic {
+
+				}
 			}
 		}
 	}
-	return grid
+}
+
+func in2(n int, t []int) bool {
+	for i := 0; i < len(t); i++ {
+		if t[i] == n {
+			return true
+		}
+	}
+	return false
 }
