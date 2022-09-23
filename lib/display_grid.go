8cb115d10ba254a2c941adc80e04fa025d356398
laremi <91208870+Godlonx@users.noreply.github.com>
2022-09-23 16:40:11 +0200

    Gestion errors and Graphic update

--- lib/display_grid.go
+++ lib/display_grid.go
@@ -4,13 +4,20 @@ import "fmt"
 
 func DisplayGrid(t [][]int) {
 	//function that display the sudoku grid in a readable format
+	print("\n\n")
 	for index_line := 0; index_line < len(t); index_line++ {
 		for index_column := 0; index_column < len(t[index_line]); index_column++ {
 			fmt.Printf("%v", t[index_line][index_column])
 			if index_column != 8 {
+				if index_column%3 == 2 {
+					fmt.Printf(" |")
+				}
 				fmt.Printf(" ")
 			}
 		}
 		fmt.Printf("\n")
+		if index_line%3 == 2 && index_line != 8 {
+			fmt.Printf("―――――――――――――――――――――\n")
+		}
 	}
 }
--- lib/grid_verification.go
+++ lib/grid_verification.go
@@ -1,1 +1,29 @@
 package lib
+
+import "fmt"
+
+func Validation(args []string) bool {
+	var (
+		arg string
+		id  int
+	)
+	if len(args) != 9 {
+		fmt.Printf("Unvalid lenght of arguments")
+		return false
+	}
+	for id, arg = range args {
+		if len(arg) != 9 {
+			fmt.Printf("Unvalid lenght of the argument %v", id)
+			return false
+		}
+		for index, val := range arg {
+			if val < '0' || val > '9' {
+				if val != '.' {
+					fmt.Printf("Invalid Number at line: %v, index: %v", id, index)
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
