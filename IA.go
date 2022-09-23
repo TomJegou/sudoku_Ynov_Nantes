7d744dfca2a42a738fe95ca744ec6fcb267b4282
laremi <91208870+Godlonx@users.noreply.github.com>
2022-09-23 10:58:50 +0200

    Advanced on IA

--- IA.go
+++ IA.go
@@ -22,7 +22,7 @@ func ResolveGrid(grid [][][]int) [][][]int {
 	}
 	if found {
 		grid[minPos[0]][minPos[1]] = []int{grid[minPos[0]][minPos[1]][0]}
-		// ReTest all possibilities for all empty cell
+		grid = Reprossessing(grid)
 		return ResolveGrid(grid)
 	} else {
 		return grid
@@ -36,4 +36,48 @@ func in2(n int, t []int) bool {
 		}
 	}
 	return false
+}
+
+func Reprossessing(grid [][][]int) [][][]int {
+	for index_line := 0; index_line < len(grid); index_line++ {
+		for index_column := 0; index_column < len(grid[index_line]); index_column++ {
+			if len(grid[index_line][index_column]) > 1 {
+				grid[index_line][index_column] = CheckPossibilities(grid, index_line, index_column)
+			}
+		}
+	}
+	return grid
+}
+
+func CheckPossibilities(grid [][][]int, line, column int) []int {
+	listVals := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	for index_line := 0; index_line < len(grid); index_line++ {
+		if index_line != line {
+			if len(grid[index_line][column]) == 1 {
+				if in2(grid[index_line][column][0], listVals) {
+					PopList(listVals, grid[index_line][column][0])
+				}
+			}
+		}
+	}
+	for index_column := 0; index_column < len(grid[0]); index_column++ {
+		if index_column != column {
+			if len(grid[line][index_column]) == 1 {
+				if in2(grid[line][index_column][0], listVals) {
+					PopList(listVals, grid[line][index_column][0])
+				}
+			}
+		}
+	}
+	return listVals
+}
+
+func PopList(list []int, val int) []int {
+	list2 := []int{}
+	for _, val2 := range list {
+		if val2 != val {
+			list2 = append(list2, val2)
+		}
+	}
+	return list2
 }
