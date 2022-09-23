1d90219ba16b90978f11b51685c4b5b53b06f8da
laremi <91208870+Godlonx@users.noreply.github.com>
2022-09-23 13:50:07 +0200

    Rename function

--- grid_verification.go
+++ grid_verification.go
@@ -1,32 +1,31 @@
 package sudoku
 
-func Testvalsudoku(ligne []int)bool { 
+func Linesudoku(ligne []int) bool {
 	lentab := len(ligne)
 	n := []int{}
-	for i := 0; i < lentab; i++ { 
+	for i := 0; i < lentab; i++ {
 		for j := 0; j < len(n); j++ {
 			if ligne[i] == n[j] { // check if i and j are the same or not, if yes, it returns false
 				return false
 			} else {
-				n = append(n, ligne[i]) //puts ligne[i] in n 
+				n = append(n, ligne[i]) //puts ligne[i] in n
+			}
 		}
-	} 
-	}	
-	return true 
+	}
+	return true
 }
 
-func Colonnesudoku (colonne [][]int, idColonne int)bool {
+func Colonnesudoku(colonne [][]int, idColonne int) bool {
 	lentab := len(colonne)
 	p := []int{}
-	for i := 0; i < lentab; i++ { 
+	for i := 0; i < lentab; i++ {
 		for j := 0; j < len(p); j++ {
 			if colonne[i][idColonne] == p[j] {
 				return false
 			} else {
-				p = append(p, colonne[i][idColonne]) 
+				p = append(p, colonne[i][idColonne])
+			}
 		}
-	} 
-	}	
-	return true 
+	}
+	return true
 }
-
