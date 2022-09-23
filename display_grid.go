95d998f2025569321c55d5e8d7706fc2d7d1389f
tomrigolo <tomjegou10@gmail.com>
2022-09-23 09:54:01 +0200

    changing display for new format

--- display_grid.go
+++ display_grid.go
@@ -6,7 +6,7 @@ func DisplayGrid(t [][][]int) {
 	//function that display the sudoku grid in a readable format
 	for index_line := 0; index_line < len(t); index_line++ {
 		for index_column := 0; index_column < len(t[index_line]); index_column++ {
-			fmt.Printf("%v", t[index_line][index_column])
+			fmt.Printf("%v", t[index_line][index_column][0])
 			if index_column != 8 {
 				fmt.Printf(" ")
 			}
