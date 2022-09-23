3d256d2e056ddd6dc5d1991fa4df74723db9d229
tomrigolo <tomjegou10@gmail.com>
2022-09-23 09:16:09 +0200

    adding a loop in order to display all the value one by one plus adding space and newline

--- display_grid.go
+++ display_grid.go
@@ -5,6 +5,12 @@ import "fmt"
 func DisplayGrid(t [][]int) {
 	//function that display the sudoku grid in a readable format
 	for index_line := 0; index_line < len(t); index_line++ {
-		fmt.Println(t[index_line])
+		for index_column := 0; index_column < len(t[index_line]); index_column++ {
+			fmt.Printf("%v", t[index_line][index_column])
+			if index_column != 8 {
+				fmt.Printf(" ")
+			}
+		}
+		fmt.Printf("\n")
 	}
 }
