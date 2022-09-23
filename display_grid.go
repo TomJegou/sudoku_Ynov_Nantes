800e3dd6a626224c9c65aea99fa0856d93711947
tomrigolo <tomjegou10@gmail.com>
2022-09-23 09:42:43 +0200

    changing display to conform whith the new format of preprocessig

--- display_grid.go
+++ display_grid.go
@@ -2,7 +2,7 @@ package sudoku
 
 import "fmt"
 
-func DisplayGrid(t [][]int) {
+func DisplayGrid(t [][][]int) {
 	//function that display the sudoku grid in a readable format
 	for index_line := 0; index_line < len(t); index_line++ {
 		for index_column := 0; index_column < len(t[index_line]); index_column++ {
